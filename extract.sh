eb4fb30e744b2ae51350f304fe11682cec5645b3
acquil98 <acquil98@gmail.com>
2020-06-03 20:29:07 +0000

    Changed shell commands

--- extract.sh
+++ extract.sh
@@ -3,7 +3,7 @@ sudo apt install netbase
 
 pip3 install whois timeout-decorator beautifulsoup4 geoip2 tldextract
 
-%shell wget https://raw.githubusercontent.com/Acquil/PhishingWebsitePredictor/master/PhishingWebsitePredictor/GeoLite2-ASN.mmdb
-%shell wget https://raw.githubusercontent.com/Acquil/PhishingWebsitePredictor/master/PhishingWebsitePredictor/GeoLite2-Country.mmdb
+wget https://raw.githubusercontent.com/Acquil/PhishingWebsitePredictor/master/PhishingWebsitePredictor/GeoLite2-ASN.mmdb
+wget https://raw.githubusercontent.com/Acquil/PhishingWebsitePredictor/master/PhishingWebsitePredictor/GeoLite2-Country.mmdb
 
 python3 extract.py
